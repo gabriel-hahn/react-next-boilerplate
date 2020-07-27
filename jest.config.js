95a9bab423535071eb184d8bb66161128df60240
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-07-27 19:25:49 -0300

    Coverage and storybooks fixes

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFromm: ['src/**/*.ts(x)'],
+  collectCoverageFromm: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 };
