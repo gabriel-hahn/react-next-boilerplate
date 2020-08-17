276db46599231cb18263a19ef77f40f524cd457f
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-08-17 19:34:18 -0300

    Storybook Essentials and Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,20 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
+  // You can use as default for every component:
+  // args: {
+  //   title: 'Default title',
+  //   description: 'Default description',
+  // }
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'Title for storybook')}
-    description={text('Description', 'Web technologies')}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+export const Default: Story = (args) => <Main {...args} />;
+
+Default.args = {
+  title: 'Default title',
+  description: 'Default description',
+};
