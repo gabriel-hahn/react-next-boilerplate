7969ac5d2fe4bcc356ca0993443edd065f2a34c2
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-03-04 09:47:55 -0300

    Husky test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,14 @@
 import * as S from './styles';
 
+const test;
+
 const Main = ({
   title = 'React - NextJS',
   description = 'TypeScript, ReactJS, NextJS e Styled Components',
 }) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React avançado escrito ao lado"
-    />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para a tela com código"
-    />
   </S.Wrapper>
 );
 
