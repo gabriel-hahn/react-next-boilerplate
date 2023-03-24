10cb666968e80f6723c5de1e763e805c4a0c51e4
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-03-23 23:21:08 -0300

    feat: update CSS styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,7 +3,7 @@ interface IMainProps {
 }
 
 export const Main = ({ title = "React - NextJS" }: IMainProps) => (
-  <main>
-    <h1 className="text-3xl font-bold underline">{title}</h1>
+  <main className="w-full h-full bg-slate-900 flex items-center justify-center">
+    <h1 className="text-5xl font-bold text-gray-300">{title}</h1>
   </main>
 );
