c00b884333aecf64cee991fba4e762c4e8b64640
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2023-03-23 23:46:38 -0300

    feat: add SSR example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
 import { Main } from "@/components/Main";
 
-export default function Home() {
+interface IHomeProps {
+  userName: string;
+}
+
+export default function Home({ userName }: IHomeProps) {
   return (
     <>
       <Head>
         <title>NextJS Boilerplate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Main />
+      <Main title={userName} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch("https://api.github.com/users/gabriel-hahn");
+  const data = await response.json();
+
+  return {
+    props: {
+      userName: data.name,
+    },
+  };
+};
